cff089a6aea5150754248b86a90e930a38c4998d
Robert Stiff <uatecuk@gmail.com>
2026-01-30 14:15:12 +0000

    try github strage y«

--- lib/private_strategy.rb
+++ lib/private_strategy.rb
@@ -1,22 +1,66 @@
 require "download_strategy"
+require "utils/github"
+require "net/http"
 
 class PrivateGitHubDownloadStrategy < CurlDownloadStrategy
   def initialize(url, name, version, **meta)
     super
+    parse_url_pattern
   end
 
-  def _curl_args
-    super + ["--header", "Authorization: token #{github_token}"]
+  def parse_url_pattern
+    url_pattern = %r{https://github.com/([^/]+)/([^/]+)/releases/download/([^/]+)/(\S+)}
+    unless @url =~ url_pattern
+      raise CurlDownloadStrategyError, "Invalid url pattern for GitHub Release."
+    end
+
+    _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
+  end
+
+  def download_url
+    uri = URI("https://api.github.com/repos/#{@owner}/#{@repo}/releases/assets/#{asset_id}")
+    req = Net::HTTP::Get.new(uri)
+    req["Accept"] = "application/octet-stream"
+    req["Authorization"] = "token #{github_token}"
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+      http.request(req)
+    end
+
+    res["location"]
   end
 
   private
+
+  def _fetch(url:, resolved_url:, timeout:)
+    # HTTP request header `Accept: application/octet-stream` is required.
+    # Without this, the GitHub API will respond with metadata, not binary.
+    curl_download download_url, "--header", "Accept: application/octet-stream", to: temporary_path
+  end
+
+  def asset_id
+    @asset_id ||= resolve_asset_id
+  end
+
+  def resolve_asset_id
+    release_metadata = fetch_release_metadata
+    assets = release_metadata["assets"].select { |a| a["name"] == @filename }
+    raise CurlDownloadStrategyError, "Asset file not found." if assets.empty?
+
+    assets.first["id"]
+  end
+
+  def fetch_release_metadata
+    release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
+    GitHub.open_api(release_url)
+  end
 
   def github_token
     @github_token ||= begin
       # Retrieve the token from the macOS Keychain
       token = `security find-generic-password -s "uatec-homebrew-tools-github-token" -w 2>/dev/null`.strip
       if token.empty?
-        raise CurlDownloadStrategyError, "No GitHub API token found in Keychain. Run `bin/setup-keys` to set it up."
+        raise CurlDownloadStrategyError, "No GitHub API token found in Keychain. Run `secure-tap-access` to set it up."
       end
       token
     end
