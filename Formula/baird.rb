8c77ef99bdb7bac05963eeccb561e7abac0aa9af
Robert Stiff <uatecuk@gmail.com>
2026-02-03 10:49:11 +0000

    don't trust ai

--- Formula/baird.rb
+++ Formula/baird.rb
@@ -5,13 +5,6 @@ class Baird < Formula
   version "v0.0.1"
   sha256 "9aba31be07a4f699b8af19efb09267c86580a7a382ede69402490676b9b09461"
 
-  # Homebrew equivalents for your list:
-  depends_on "mesa"       # Provides gbm, glapi, dri, egl
-  depends_on "libdrm"     # Provides libdrm2
-  depends_on "libinput"   # Provides libinput10
-  depends_on "icu4c"      # Provides libicu
-  depends_on "mpv"        # Provides libmpv-dev headers and libs
-  
   # If this is for Linux specifically
   depends_on :linux
 
