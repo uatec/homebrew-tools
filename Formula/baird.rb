83bb0021b7868d1b94e3fd466f7ef3f0f68d3023
Robert Stiff <uatecuk@gmail.com>
2026-02-03 10:58:13 +0000

    note sure these dependencies work like this

--- Formula/baird.rb
+++ Formula/baird.rb
@@ -9,15 +9,15 @@ class Baird < Formula
   sha256 "9aba31be07a4f699b8af19efb09267c86580a7a382ede69402490676b9b09461"
 
 
-    depends_on "libgbm1"
-    depends_on "libdrm2"
-    depends_on "libinput10"
-    depends_on "libglapi-mesa"
-    depends_on "libgl1-mesa-dri"
-    depends_on "libegl-mesa0"
-    depends_on "libegl1"
-    depends_on "libicu-dev"
-    depends_on "libmpv-dev"
+    # depends_on "libgbm1"
+    # depends_on "libdrm2"
+    # depends_on "libinput10"
+    # depends_on "libglapi-mesa"
+    # depends_on "libgl1-mesa-dri"
+    # depends_on "libegl-mesa0"
+    # depends_on "libegl1"
+    # depends_on "libicu-dev"
+    # depends_on "libmpv-dev"
 
   # If this is for Linux specifically
   depends_on :linux
