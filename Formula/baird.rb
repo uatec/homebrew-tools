36adcdab7c59781cd567ba77b55454a6db600e3e
Robert Stiff <uatecuk@gmail.com>
2026-02-03 10:51:07 +0000

    trial and error

--- Formula/baird.rb
+++ Formula/baird.rb
@@ -4,7 +4,7 @@ class Baird < Formula
   desc "Fully integrated tv and streaming media player"
   homepage "https://github.com/uatec/baird"
   url "https://github.com/uatec/baird/releases/download/v0.0.1/Baird-linux-arm64.zip"
-    using: PrivateGitHubDownloadStrategy
+      using: PrivateGitHubDownloadStrategy
   version "v0.0.1"
   sha256 "9aba31be07a4f699b8af19efb09267c86580a7a382ede69402490676b9b09461"
 
